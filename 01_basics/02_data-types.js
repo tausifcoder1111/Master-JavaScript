48d84e8e845793d281ee191181f7dba5ebea5045
tausifcoder1111 <tausifcoderr@gmail.com>
2026-03-19 12:34:39 +0530

    datatypes

---
+++ 01_basics/02_data-types.js
@@ -0,0 +1,14 @@
+let value = "33"
+console.log(value)
+console.log(typeof(value))
+
+let ValueInNumber = Number(value)
+console.log(ValueInNumber)
+console.log(typeof(ValueInNumber))
+
+let value1 = "3312dws"
+let ValueInNumber1 = Number(value1)
+console.log(ValueInNumber1)
+console.log(typeof(ValueInNumber1))
+
+console.log(typeof(NaN));
